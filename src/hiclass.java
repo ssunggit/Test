153744ec1788027d4594846333e958c676bae1d8
ssunggit <gustjd68@gmail.com>
2022-03-07 14:51:09 +0900

    gg

--- src/hiclass.java
+++ src/hiclass.java
@@ -6,6 +6,8 @@ public class hiclass {
 		System.out.println("Test");
 		System.out.println("Test1");
 		System.out.println("Test2");
+		System.out.println("Test3");
+		System.out.println("Test4");
 	}
 
 }
